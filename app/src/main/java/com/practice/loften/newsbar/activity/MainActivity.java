f4612d7e0c4afe30d12cc5dbd06139ba087cb8b7
Loften <93663469@qq.com>
2015-12-02 00:18:57 +0800

    Initial commit

---
+++ app/src/main/java/com/practice/loften/newsbar/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.practice.loften.newsbar.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.practice.loften.newsbar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
